b0dc236faac776f06bdf98dee1f1ef4930d22ff0
Yeona Hong <yeonahong@dsp.yonsei.ac.kr>
2024-08-19 15:38:29 +0900

    [ADD] 튜토리얼 준비

---
+++ init.sh
@@ -0,0 +1,12 @@
+cd ../
+mv ECAPA-TDNN TensorRT/tools/pytorch-quantization/tests
+cd TensorRT/tools/pytorch-quantization/
+
+pip install -r requirements.txt
+pip install torch==2.2.1 --index-url https://download.pytorch.org/whl/cu121
+python setup.py install
+pip install soundfile
+pip install scikit-learn
+pip install torchaudio==2.2.1
+pip install tqdm
+pip install tensorboardX
