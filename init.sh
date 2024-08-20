51bf30e10b8c81329cea4a119a120350044b61cc
Yeona Hong <yeonahong@dsp.yonsei.ac.kr>
2024-08-20 16:23:28 +0900

    [ADD] init file update

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 cd ../
 mv ECAPA-TDNN TensorRT/tools/pytorch-quantization/tests
 cd TensorRT/tools/pytorch-quantization/
+mkdir models
 
 pip install -r requirements.txt
 pip install torch==2.2.1 --index-url https://download.pytorch.org/whl/cu121
@@ -10,3 +11,6 @@ pip install scikit-learn
 pip install torchaudio==2.2.1
 pip install tqdm
 pip install tensorboardX
+pip install onnx
+#pip install onnxruntime-gpu --extra-index-url https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/onnxruntime-cuda-12/pypi/simple/
+
